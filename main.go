7477ba59c84378aacf8c1bc9089607b72897e3be
Arif Ikhsanudin <32485694+arifikhsan@users.noreply.github.com>
2017-11-15 23:21:16 +0700

    komit kedua

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "github.com/arifikhsan/bee/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/auth"
+)
+
+func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, auth.Basic("username", "secretpassword"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
